a5607c3efb2231b8c65c56e201e799c568ab98ee
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-05-10 16:11:23 +0530

    socket connection for client and server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
+import { SocketProvider } from './context/SocketContext'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
-    <Toaster/>
+    <SocketProvider>
+      <App />
+      <Toaster/>
+    </SocketProvider>
   </>
 )
