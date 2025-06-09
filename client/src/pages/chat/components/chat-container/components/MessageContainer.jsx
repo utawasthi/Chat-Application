f52f47a9c7268a8591f999d4509e4e1092c51d72
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-06-10 01:28:57 +0530

    display msg in personal chat done

--- client/src/pages/chat/components/chat-container/components/MessageContainer.jsx
+++ client/src/pages/chat/components/chat-container/components/MessageContainer.jsx
@@ -1,7 +1,64 @@
+import { useAppStore } from "@/store";
+import moment from "moment";
+import { useEffect, useRef } from "react"
+
+
 const MessageContainer = () => {
+
+  const scrollRef = useRef();
+  const {selectedChatData , selectedChatType , userInfo , selectedChatMessages } = useAppStore();
+
+  useEffect(() => {
+    if(scrollRef.current){
+      scrollRef.current.scrollIntoView({behavior : 'smooth'});
+    }
+  } , [selectedChatMessages]);
+
+
+  const renderDmMessages = (msg) => {
+    return (
+      <div className = {`${msg.sender === selectedChatData._id ? 'text-left' : 'text-right'}`}>
+        {
+          msg.msgType === 'text' && (
+            <div className = {`${msg.sender !== selectedChatData._id ? 
+              "bg-[#8417ff]/5  text-[#8217ff]/90 border-[#8417ff]/50" :  
+              "bg-[#2a2b33]/5  text-white/90 border-[#ffffff]/20"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`} >
+              {msg.content}
+            </div>
+          )
+        }
+        <div className = 'text-xs text-gray-600'>
+          {moment(msg.timestamp).format("LT")}
+        </div>
+    </div>
+    )
+  }
+
+  const renderMessages = () => {
+    let lastDate = null;
+    return selectedChatMessages.map((msg , index) => {
+      const msgDate = moment(msg.timestamp).format("YYYY-MM-DD");
+      const showDate = msgDate !== lastDate;
+      lastDate = msgDate;
+      return (
+        <div key = {msg._id}>
+           {showDate && (
+            <div className = 'text-center text-gray-500 my-2'>
+              {moment(msg.timestamp).format("LL")}
+            </div>
+           )}
+           {
+            selectedChatType === 'contact' && renderDmMessages(msg)
+           }
+        </div>
+      )
+    })
+  }
+
   return (
     <div className = 'flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full'>
-      Message Container
+      {renderMessages()}
+      <div ref = {scrollRef}/>
     </div>
   )
 }
